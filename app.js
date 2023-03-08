f81f5673ac19932ade36b3f0d3c5a26ede9fa8e5
AndresBaron21 <AndresBaron2018@outlook.com>
2023-03-08 13:24:40 -0500

    test deploy

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-// const express = require('express');
-// const app = express();
-// const router = require('./src/routes/apis');
-// const cors = require('cors');
+const express = require('express');
+const app = express();
+const router = require('./src/routes/apis');
+const cors = require('cors');
 
 
-// app.use(cors());
-// // Add the prefix "/api" to all application routes
-// app.use('/api', router);
+app.use(cors());
+// Add the prefix "/api" to all application routes
+app.use('/api', router);
 
-// // Set the port on which the server will run
-// const PORT = process.env.PORT || 3001;
+// Set the port on which the server will run
+const PORT = process.env.PORT || 3001;
 
-// // We start the server
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
-
-
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('hello world')
-})
-
-server.listen(3001)
-console.log(`Server started on port 3001`);
+// We start the server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
 
