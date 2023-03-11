e3b3feacc28d2a562cc04f66c9e3583dfe0c7dbf
AndresBaron21 <AndresBaron2018@outlook.com>
2023-03-11 10:18:20 -0500

    Fixed a bug

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,18 +3,26 @@ const secretKey = 'mySecretKey';
 
 const authMiddleware = (req, res, next) => {
     // Get the JWT token from the request headers
-    const token = req.rawHeaders[1]; // Get the token from the Authorization header
+    // const token = req.rawHeaders[1]; // Get the token from the Authorization header
     
+    let headers = {};
+    req.rawHeaders.forEach((value, index) => {
+      if (index % 2 === 0) {
+        headers[value.toLowerCase()] = req.rawHeaders[index + 1];
+      }
+    });
+    const token = headers.authorization;
+
     try {
-      //   // Verify the token and extract the user's roles
-      // const decoded = jwt.verify(token, secretKey);
-      // const roles = decoded.roles;
+        // Verify the token and extract the user's roles
+      const decoded = jwt.verify(token, secretKey);
+      const roles = decoded.roles;
       
   
-      // // Check if the user is an admin
-      // if (!roles.includes('admin')) {
-      //   return res.status(401).send('Unauthorized');
-      // }
+      // Check if the user is an admin
+      if (!roles.includes('admin')) {
+        return res.status(401).send('Unauthorized');
+      }
   
       // If the user is authorized, call the next middleware
       next();
